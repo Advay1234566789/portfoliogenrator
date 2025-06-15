2a4d5ed6984692a88b7d45d04eaf2c80d797850e
Advay <advay@email.com>
2025-06-15 11:49:00 +0530

    Add all project files

---
+++ src/components/Javascript.js
@@ -0,0 +1,24 @@
+import bootstrap from "bootstrap";
+
+window.addEventListener("DOMContentLoaded", () => {
+  // Activate Bootstrap scrollspy on the main nav element
+  const sideNav = document.body.querySelector("#sideNav");
+  if (sideNav) {
+    new bootstrap.ScrollSpy(document.body, {
+      target: "#sideNav",
+      offset: 74,
+    });
+  }
+
+  // Collapse responsive navbar when toggler is visible
+  const navbarToggler = document.body.querySelector(".navbar-toggler");
+  const responsiveNavItems = document.querySelectorAll("#navbarResponsive .nav-link");
+
+  responsiveNavItems.forEach((responsiveNavItem) => {
+    responsiveNavItem.addEventListener("click", () => {
+      if (window.getComputedStyle(navbarToggler).display !== "none") {
+        navbarToggler.click();
+      }
+    });
+  });
+});
